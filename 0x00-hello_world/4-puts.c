fcf99ff821d92273ffd8605d628e5f6f8f13c698
preciousNu <nprecious533@gmail.com>
2023-02-16 13:00:43 -0800

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle, followed by newline."
+ * Return : Always 0 (success)
+ */
+init main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
