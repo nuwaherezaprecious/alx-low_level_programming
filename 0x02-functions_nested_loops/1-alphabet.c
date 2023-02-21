e34371686cf143ee7d616c3cc64c15afcbe96dcb
preciousNu <nprecious533@gmail.com>
2023-02-21 14:13:43 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
