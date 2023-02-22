ca2c13e1b9705fcdbf41fae1f2fab67d98c4850f
preciousNu <nprecious533@gmail.com>
2023-02-22 15:44:29 -0800

    add10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,15 @@
  
 /**
  * add - add two values
- * @n1 - the first number to be computed
- * @n2 - the second number to be computed
+ * @n1: the first number to be computed
+ * @n2: the second number to be computed
  * Return: integar value
  */
 
 int add(int n1, int n2)
 
 {
+
 	return (n1 + n2);
+
 }
--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -3,15 +3,15 @@
 
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
 
 int main(void)
-{
-    int n;
 
-    n = add(89, 9);
-    printf("%d\n", n);
-    return (0);
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
 }
