0a35047de9c1e5dd92e2bb9e90a1760fe3b3085b
preciousNu <nprecious533@gmail.com>
2023-03-21 13:32:51 -0700

    object like

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,2 +1,4 @@
-#define _HEADER_
+#ifndef _OBJECT_LIKE_MARCO_H_
+#define _OBJECT_LIKE_MARCO_H_
 #define SIZE 1024
+#endif /*_OBJECT_LIKE_MARCO_H_*/
--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,2 +1,4 @@
-#define _HEADER_
+#ifndef _PI_H
+#define _PI_H
 #define PI 3.14159265359
+#endif
